557b68f9bd024af992bcb148d6a5f0d499faa6f6
azcera <azcera@vk.com>
2026-03-30 15:06:10 +0300

    fix createButtons.js

--- commands/utility/createButtons.js
+++ commands/utility/createButtons.js
@@ -1,28 +1,31 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { navigationButttons } = require("../../config.json");
+
+function getComponents() {
+  let components = [];
+  let j = 0;
+  row = [];
+  for (let i = 0; i < navigationButttons.length; i++) {
+    if (j === 4) {
+      j = 0;
+      components = [...components, new ActionRowBuilder().addComponents(row)];
+      row = [];
+    }
+    row = [
+      ...row,
+      new ButtonBuilder()
+        .setLabel(navigationButttons[i].label)
+        .setStyle(ButtonStyle.Link)
+        .setURL(navigationButttons[i].link),
+    ];
+    j++;
+  }
+  if (row.length > 0) {
+    components = [...components, new ActionRowBuilder().addComponents(row)];
+  }
+  return components;
+}
 
 module.exports = {
-  getComponents() {
-    let components = [];
-    let j = 0;
-    row = [];
-    for (let i = 0; i < navigationButttons.length; i++) {
-      if (j === 4) {
-        j = 0;
-        components = [...components, new ActionRowBuilder().addComponents(row)];
-        row = [];
-      }
-      row = [
-        ...row,
-        new ButtonBuilder()
-          .setLabel(navigationButttons[i].label)
-          .setStyle(ButtonStyle.Link)
-          .setURL(navigationButttons[i].link),
-      ];
-      j++;
-    }
-    if (row.length > 0) {
-      components = [...components, new ActionRowBuilder().addComponents(row)];
-    }
-    return components;
-  },
+  getComponents,
 };
