0e7534a7a862df9331125030769b0426c8cc6a32
azcera <azcera@vk.com>
2026-03-30 16:19:20 +0300

    fix splitname3

--- commands/utility/splitName.js
+++ commands/utility/splitName.js
@@ -1,13 +1,14 @@
 function splitName(nickname) {
+  // Разделяем на части по ' | '
   const splittedName = nickname.split(" | ");
   if (splittedName.length < 2) return null;
 
-  const match = nickname.match(/(?:\] )?([^|]+)/);
+  const match = nickname.match(/\]?\s*([^\|]+)/);
 
   return {
     name: match
-      ? match[1].toLowerCase().trim()
-      : splittedName[0].toLowerCase().trim(),
+      ? match[1].trim().toLowerCase()
+      : splittedName[0].trim().toLowerCase(),
     stat: splittedName[1].trim(),
   };
 }
