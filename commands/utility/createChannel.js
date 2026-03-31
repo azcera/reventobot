c81825936e04c397b5009fa2703a95d91e1799e9
azcera <109483709+azcera@users.noreply.github.com>
2026-03-31 08:54:18 +0000

    add channel roles

--- commands/utility/createChannel.js
+++ commands/utility/createChannel.js
@@ -1,23 +1,40 @@
-const { MessageFlags } = require('discord.js')
+const { MessageFlags } = require("discord.js");
+const { adminRoles } = require("../../config.json");
 
-async function createChannel (interaction, {channelName, memberID, guild, categoryID})  {
+async function createChannel(
+    interaction,
+    { channelName, memberID, guild, categoryID },
+) {
     const newChannel = await guild.channels.create({
         name: channelName,
         type: 0,
         parent: categoryID,
         permissionOverwrites: [
-                {
-                    id: guild.roles.everyone.id,
-                    deny: ['ViewChannel'],
-                },
-                {
-                    id: memberID,
-                    allow: ['ViewChannel', 'SendMessages', 'ReadMessageHistory'],
-                },
-            ],
-    })
-    await interaction.reply({ content: `Архив для <@${memberID}> создан - ${newChannel}`, flags: MessageFlags.Ephemeral });
+            {
+                id: guild.roles.everyone.id,
+                deny: ["ViewChannel"],
+            },
+            {
+                id: memberID,
+                allow: ["ViewChannel", "SendMessages", "ReadMessageHistory"],
+            },
+            adminRoles.map((roleId) => {
+                return {
+                    id: roleId,
+                    allow: [
+                        "ViewChannel",
+                        "SendMessages",
+                        "ReadMessageHistory",
+                    ],
+                };
+            }),
+        ],
+    });
+    await interaction.reply({
+        content: `Архив для <@${memberID}> создан - ${newChannel}`,
+        flags: MessageFlags.Ephemeral,
+    });
     return newChannel;
-} 
+}
 
-module.exports = { createChannel }
+module.exports = { createChannel };
