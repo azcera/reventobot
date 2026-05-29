f0f38fbafd290499e6139aa7f0cb9652862cbedb
azcera <azcera@vk.com>
2026-05-29 11:07:35 +0300

    fix oncreate channel

--- events/onCreateChannel.js
+++ events/onCreateChannel.js
@@ -1,11 +1,15 @@
 const { sendArchiveMessage } = require("../commands/utility/archiveMessage");
 require("dotenv").config();
 
+const categoryIds = process.env.CATEGORY_IDS
+  ? process.env.CATEGORY_IDS.split(",").map((id) => id.trim())
+  : [];
+
 module.exports = (client) => {
   client.on("channelCreate", async (channel) => {
     if (channel.isTextBased()) {
-      if (channel.parentId !== process.env.CATEGORY_ID) return;
-      sendArchiveMessage(channel);
+      if (!categoryIds.includes(channel.parentId)) return;
+      await sendArchiveMessage(channel);
     }
   });
 };
