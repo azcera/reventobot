35e9c4276a96748813a3615de3867710b181326d
azcera <azcera@vk.com>
2026-09-10 01:34:39 +0300

    add updating archives lists in thread create/delete events

--- src/events/onThreadDelete.js
+++ src/events/onThreadDelete.js
@@ -2,6 +2,7 @@ const { Events, Client } = require('discord.js')
 const {
 	updateUnansweredList
 } = require('../features/archive/archiveUnansweredList')
+const { updateArchivesList } = require('../features/archive/archivesList')
 
 require('dotenv').config()
 
@@ -12,6 +13,7 @@ require('dotenv').config()
 module.exports = client => {
 	client.on(Events.ThreadDelete, async thread => {
 		if (thread.parentId === process.env.PARENT_CHANNEL_ID) {
+			await updateArchivesList(thread.guild)
 			await updateUnansweredList(thread.guild)
 		}
 	})
